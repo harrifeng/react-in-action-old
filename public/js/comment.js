9d81e3ef0c798a8428d66a6037fa4e5c7b5b9cc0
i309511 <harrifeng@gmail.com>
2015-12-13 01:23:05 -0800

    structure the data

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,19 +3,29 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
             <h1>Comments</h1>
-            <CommentList />
+            <CommentList data={this.props.data}/>
             <CommentForm />
             </div>
         );
     }
 });
 
+var data = [
+    {id:1, author: "Pete Hunt", text: "This is one comment!"},
+    {id:2, author: "Jordan Walke", text: "This is *another* comment!"}
+];
 var CommentList = React.createClass({
     render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-            <Comment author="Pete Hunt">This is one comment</Comment>
-            <Comment author="Jordaan Walke">This is *another* comment </Comment>
+            {commentNodes}
             </div>
         );
     }
@@ -49,6 +59,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={data}/>,
     document.getElementById('content')
 );
